567980a9cc9a3590cda95e2468dce9c9bfcdbc50
Camberden <42500247+Camberden@users.noreply.github.com>
2025-12-09 12:58:07 -0500

    included number variable file

--- latestupdate.cbl
+++ latestupdate.cbl
@@ -1,7 +1,13 @@
-       IDENTIFICATION DIVISION.
-       PROGRAM-ID. latestupdate.
-       ENVIRONMENT DIVISION.
-       PROCEDURE DIVISION.
-           DISPLAY "December 9th, 2025".
-           STOP RUN.
+*******===> COBOL PLAY 2025 ===>
+******* GnuCobol 3.2.0
+******* Practice Sessions
+******* Latest Update: December 9th, 2025
+*******
+000100 IDENTIFICATION DIVISION.
+000200     PROGRAM-ID. latestupdate.
+000300 ENVIRONMENT DIVISION.
+000400 PROCEDURE DIVISION.
+000500     DISPLAY "December 9th, 2025".
+000600     STOP RUN.
+000700 
 		   
